404877c4ba89140232aa91daa2c623d1d20f59b5
John Dohoney Jr <johndohoneyjr@Blackhawk.local>
2019-01-08 13:17:26 -0800

    test of sensitive outputs--no affect to any dependent workspaces

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,10 @@ output "public_sg" {
 output "subnet_ips" {
   value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
 }
+
+// testing
+//
+output "security_group" {
+  sensitive = true
+  value = "${aws_security_group.tf_public_sg.id}"
+}

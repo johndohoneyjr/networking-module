c15425fc6a6f9296b17dad155b0f9dda5a21bada
John Dohoney Jr <johndohoneyjr@Blackhawk.local>
2019-01-08 16:41:12 -0800

    added back in foo2

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,18 @@
 ## -- Networking/outputs
 
 output "public_subnets" {
-  value = "${aws_subnet.tf_public_subnet.*.id}"
+//  value = "${aws_subnet.tf_public_subnet.*.id}"
+  value = "1"
 }
 
 output "public_sg" {
-  value = "${aws_security_group.tf_public_sg.id}"
+//  value = "${aws_security_group.tf_public_sg.id}"
+    value = "2"
 }
 
 output "subnet_ips" {
-  value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
+//  value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
+    value = "3"
 }
 
 // testing

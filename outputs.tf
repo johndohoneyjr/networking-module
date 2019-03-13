34b411823de95dd8340c1bd03daae668e2769a2b
John Dohoney Jr <johndohoneyjr@Blackhawk.local>
2019-03-12 17:38:05 -0700

    Took out Experimentation Code

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,14 @@
 ## -- Networking/outputs
 
 output "public_subnets" {
-//  value = "${aws_subnet.tf_public_subnet.*.id}"
-  value = "1"
+  value = "${aws_subnet.tf_public_subnet.*.id}"
 }
 
 output "public_sg" {
-//  value = "${aws_security_group.tf_public_sg.id}"
-    value = "2"
-}
-
-output "subnet_ips" {
-//  value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
-    value = "3"
-}
-
-// testing
-//
-output "securitygroup" {
   value = "${aws_security_group.tf_public_sg.id}"
 }
 
-output "foo" {
-  sensitive = true
-  value = "bar"
+output "subnet_ips" {
+  value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
 }
 
-output "foo2" {
-  sensitive = true
-  value = "bar2"
-}

aad4f4a2626dce86d551452290571b9fae1db653
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2019-06-10 11:34:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "tf_vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags {
+  tags = {
     Name = "${terraform.workspace}-${var.project_name}"
   }
 }
@@ -26,7 +26,7 @@ resource "aws_vpc" "tf_vpc" {
 resource "aws_internet_gateway" "tf_igw" {
   vpc_id = "${aws_vpc.tf_vpc.id}"
 
-  tags {
+  tags = {
     Name = "${var.project_name}"
   }
 }
@@ -47,7 +47,7 @@ resource "aws_route_table" "tf_public_rt" {
 resource "aws_default_route_table" "tf_private_rt" {
   default_route_table_id = "${aws_vpc.tf_vpc.default_route_table_id}"
 
-  tags {
+  tags = {
     Name = "${var.project_name}"
   }
 }
@@ -61,7 +61,7 @@ resource "aws_subnet" "tf_public_subnet" {
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
 
-  tags {
+  tags = {
     Name = "${var.project_name}-${count.index + 1}"
   }
 }
